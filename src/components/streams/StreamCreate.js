baad9a88f5eb10b6270c481b40fc21a336526480
awe peter <awe.petersq@gmail.com>
2022-02-03 03:59:45 -0800

    streams

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 
-const StreamCreate = () => {
-    return<div>
-           Stream Create
-    </div>
+
+class StreamCreate extends React.Component {
+    renderInput({input, label}){
+        return (
+         
+        <div className='field'>
+               <label> {label} </label>
+            <input {...input} />
+        </div>
+        );
+    }
+
+    onSubmit(formValues){
+        console.log(formValues);
+    };
+
+    render(){
+        return(
+              <form 
+               onSubmit={this.props.handleSubmit(this.onSubmit)}
+               className='ui form'>
+                  <Field name = "title" component = {this.renderInput} label = " Enter title"/>
+                  <Field name = "description"  component = {this.renderInput} label = "Enter description"/>
+                  <button className='ui button primary'>submit</button>
+              </form>
+        );
+    }
+}
+
+const validate = (formValues) => {
+const errors = {};
+if (!formValues.title){
+    errors.title = 'you must enter a title';
+}
+if(!formValues.description){
+    errors.title = 'you must enter a description'
+}
+
+return errors;
+
 };
 
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
 
-export default StreamCreate;
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import AuthReducer from './AuthReducer';
 
 
 
 export default combineReducers({
-    auth: AuthReducer
-})
+    auth: AuthReducer,
+    form: formReducer
+});
+
